b1182c713db229e349322b56bf88e5cec0136a11
Tanuka16 <das.tanuka@yahoo.com>
2019-06-04 16:38:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,13 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse          # instance variable @reversed_string lets the contents become 'visible' to the erb file to which it renders
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
-
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :friends
   end
 end
